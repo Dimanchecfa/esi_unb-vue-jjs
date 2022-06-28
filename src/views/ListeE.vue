09bd9cc4e4962cdef01ee5f9612d9035ccd93c48
Dimanchecfa <dimanchenamo@gmail.com>
2022-06-28 15:45:58 +0200

    methode post

--- src/views/ListeE.vue
+++ src/views/ListeE.vue
@@ -1,4 +1,5 @@
 <template>
+
 <div v-if="loading">
  <div class="flex justify-center items-center space-x-2">
   <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-blue-600" role="status">
@@ -33,8 +34,10 @@
   </div>
 </div>
 </div>
+
     <!-- component -->
 <div v-else>
+  
      <h1 class="font-bold text-2xl text-gray-700 w-8/10 text-center">
       Liste des  secretaire
           </h1>
@@ -65,23 +68,23 @@
         <tbody class="bg-gray-200">
           <tr :key="student.id" v-for="student in students" class="bg-white border-4 border-gray-200 text-center">
            
-            <td class="px-16 py-2 text-center text-center">
+            <td class="px-2 py-2 text-center text-center">
               <span class="text-center  font-semibold">{{student.matricule}}</span>
             </td>
            
-            <td class="px-16 py-2 text-center text-center">
-              <span class="text-center  font-semibold">{{student.nom}}</span>
+            <td class="px-2 text-center  font-semibold">
+              <span class="text-center  font-semibold" >{{student.nom}}</span>
             </td>
            
-            <td class="px-16 py-2 text-center text-center">
+            <td class="px-2 py-2 text-center text-center">
               <span class="text-center  font-semibold">{{student.prenom}}</span>
             </td>
-            <td class="px-16 py-2 text-center text-center">
+            <td class="px-2 py-2 text-center text-center">
               <span class="text-center  font-semibold">{{student.email}}</span>
             </td>
            
-             <td class="px-16 py-2 text-center">
-              <font-awesome-icon icon="far fa-edit" />
+             <td class="px-2 py-2 text-center flex space-x-2 ml-5">
+             <button class="btn btn-danger btn-sm" @click="deleteProduct(student.id)">Delete</button> 
             </td>
 
            
@@ -99,7 +102,8 @@ export default {
     data() {
       return{
           students:[],
-          loading:true
+          loading:true,
+         
       }
     },
   methods :{
@@ -109,15 +113,27 @@ export default {
         .then( (response)=> {
             this.students = response.data.data,
             this.loading = false
+           
         })
         .catch( (error)=> {
             console.log(error.toJSON())
         })
         
+    },
+     deleteProduct() {
+        axios.delete(`http://127.0.0.1:8000/api/student/${student.id}`)
+             .then(response => {
+                 console.log(response);
+             })
+             .catch(function (error) {
+                console.log(error.response)
+             })
     }
+   
     },
     mounted() {
         this.getStudent();
+        
     }
 
 }
